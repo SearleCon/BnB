893069366cba9775cbfd466d3be14078c5aacded
Paul Janse van Rensburg <paul@searleconsulting.co.za>
2012-07-25 15:34:03 +0200

    testchange

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Shell App!"
       redirect_to @user
     else
       render 'new'

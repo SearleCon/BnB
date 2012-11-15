8d40f0ba95cf09b41812853178954557056e6084
Paul Janse van Rensburg <paul@searleconsulting.co.za>
2012-11-15 15:53:52 +0200

    Laatest

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ SampleApp::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
 
   # Action Mailer

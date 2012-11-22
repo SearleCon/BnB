ba6cf258cca9834f410540a1a0de9c00d526180a
Paul Janse van Rensburg <paul@searleconsulting.co.za>
2012-11-22 15:33:14 +0200

    fresh database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Role.create(description: "Owner")
-Role.create(description: "Guest")
+Role.create(description: "guest")
+Role.create(description: "owner")
+
+
+Plan.create(duration: 1, price: 0, active: true, name: '30 Day Trial', free: true)
+Plan.create(duration: 1, price: 50, active: true, name: 'Bronze Package', free: false)
+Plan.create(duration: 3, price: 150, active: true, name: 'Silver Package', free: false)
+Plan.create(duration: 6, price: 300, active: true, name: 'Gold Package', free: false)
+Plan.create(duration: 12, price: 600, active: true, name: 'Platinum Package', free: false)
+
+
+
+
+
